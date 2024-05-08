24aea645efcacdb98b6e65c39a59c41749d9b586
ik012982i9 <a@a.a>
2024-05-08 08:55:24 +0200

    Documentacion de la clase Alumno.java

--- src/main/java/com/ejemplo/clases/Alumno.java
+++ src/main/java/com/ejemplo/clases/Alumno.java
@@ -1,21 +1,53 @@
+/**
+ * Representa a un alumno en un curso.
+ *
+ * @author [Tu nombre]
+ */
 package com.ejemplo.clases;
 
 public class Alumno {
+    /**
+     * Identificador del alumno.
+     */
     private String identificador;
+    /**
+     * Nombre del alumno.
+     */
     private String nombre;
+    /**
+     * Curso en el que se encuentra el alumno.
+     */
     private String curso;
 
-    // Constructor
+    /**
+     * Constructor de la clase Alumno.
+     *
+     * @param identificador Identificador del alumno.
+     * @param nombre        Nombre del alumno.
+     * @param curso         Curso en el que se encuentra el alumno.
+     */
     public Alumno(String identificador, String nombre, String curso) {
         this.identificador = identificador;
         this.nombre = nombre;
         this.curso = curso;
     }
 
+    /**
+     * Matricula al alumno en un curso.
+     * Agrega un uno a el nombre del curso
+     * El curso no debe estar vacio.
+     *
+     * @param curso Curso en el que se matricula el alumno.
+     */
     public void matricular(String curso){
         this.curso = curso + " 1";
     }
 
+    /**
+     * Avanza el curso del alumno.
+     *
+     * Si el alumno está en el primer curso, pasa al segundo. Si ya está en el último curso, se titula.
+     */
     public void pasarDeCurso(){
         String[] curso = this.curso.split(" ");
         int numeroCurso = Integer.parseInt(curso[1]);
@@ -29,7 +61,6 @@ public class Alumno {
 
     }
 
-    // Getters y Setters
     public String getIdentificador() {
         return identificador;
     }
